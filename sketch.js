b39afe3df3daef4c20bd5efb0a5d9ad1099dadc6
MohamedRazeen102006 <mohd.rizwan@gmail.com>
2020-06-18 20:43:50 +0530

    Integrated game with database

--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
-var ball;
+var ball, database, ref, positionData;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
-}
+    //Alias for firebase.database()
+    database = firebase.database();
 
+    ref = database.ref("Ball/position");
+    ref.on('value',getData,errData);
+}
+function getData(data){
+    positionData = data;
+}
+function errData(err){
+    console.log(err);
+}
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
@@ -20,10 +30,18 @@ function draw(){
     else if(keyDown(DOWN_ARROW)){
         changePosition(0,+1);
     }
+    ball.x = positionData.val().x;
+    ball.y = positionData.val().y;
     drawSprites();
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    let ballpos = positionData.val();
+    /*ball.x = ball.x + x;
+    ball.y = ball.y + y;*/
+    //console.log(ballpos,x,y);
+    ref.set({
+        x:ballpos.x+x,
+        y:ballpos.y+y
+    });
 }
